d0275d23e742ffdc3d37ab0e034fa5e24e86cc1b
jinmu1999111 <miniwagon111@gmail.com>
2025-07-18 21:05:05 +0900

    change sql appoit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import datetime
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 # Renderの環境変数からデータベースURLを読み込む
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
